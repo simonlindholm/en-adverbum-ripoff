c667fa5f9d2ba9512b0051540cd0fa4683d7aa05
Simon Lindholm <simon.lindholm10@gmail.com>
2012-12-02 14:30:00 +0100

    Allow non-targetted target commands.
    E.g. examining nothing = the room.

--- TargettedCommand.java
+++ TargettedCommand.java
@@ -10,19 +10,17 @@ public abstract class TargettedCommand extends Command {
 	public abstract boolean maybePerformWith(Game game, String target);
 
 	public boolean maybePerform(String input, Game game) {
-		assert (this.group != 0) != (this.thing != null) : "Must have exactly one target.";
+		assert (this.group == 0) || (this.thing == null) : "Must have at most one target.";
 		assert !(this.group != 0 && this.regex == null) : "Can't use capture groups without regex.";
 
 		String target = this.thing;
-		if (this.regex != null) {
+		if (this.regex != null && this.group != 0) {
 			Pattern p = Pattern.compile(this.regex);
 			Matcher m = p.matcher(input);
 			boolean found = m.find();
 			assert found;
-			if (this.group != 0) {
-				assert this.group <= m.groupCount();
-				target = m.group(this.group);
-			}
+			assert this.group <= m.groupCount();
+			target = m.group(this.group);
 		}
 		return this.maybePerformWith(game, target);
 	}
